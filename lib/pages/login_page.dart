75221967f2900bf685e29945ab76bb58e1976618
Martin <jmartinvz@outlook.com>
2020-10-27 15:01:09 -0500

    Primer commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,77 @@
+import 'package:chat_app/widgets/blue_button.dart';
+import 'package:chat_app/widgets/custom_input.dart';
+import 'package:chat_app/widgets/label.dart';
+import 'package:chat_app/widgets/logo.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Logo(titulo: 'Messenger'),
+                  _Form(),
+                  Labels(
+                      titulo: 'No tienes una cuenta?',
+                      subtitulo: 'Crea una ahora!',
+                      routeName: 'register'),
+                  Text(
+                    'Terminos y condiciones de uso',
+                    style: TextStyle(fontWeight: FontWeight.w200),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Password',
+            textController: passCtrl,
+            isPassword: true,
+          ),
+          BlueButton(
+            text: 'Ingrese',
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
